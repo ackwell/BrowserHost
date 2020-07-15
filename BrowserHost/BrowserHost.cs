9abf261f8d72a3c46211da885ffc5d94f7dfa52d
ackwell <saxon@ackwell.com.au>
2020-07-15 23:41:05 +1000

    Initial plugin scaffold

---
+++ BrowserHost/BrowserHost.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud.Plugin;
+
+namespace BrowserHost
+{
+    public class BrowserHost : IDalamudPlugin
+    {
+        public string Name => "Browser Host";
+
+        private DalamudPluginInterface pluginInterface;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pluginInterface = pluginInterface;
+
+            PluginLog.Log("BrowserHost loaded.");
+        }
+
+        public void Dispose()
+        {
+            pluginInterface.Dispose();
+        }
+    }
+}
