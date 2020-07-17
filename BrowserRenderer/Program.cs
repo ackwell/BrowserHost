449b25c13d2318a340e07c3beba8f567f586b798
ackwell <saxon@ackwell.com.au>
2020-07-17 20:37:03 +1000

    CEF initialised

--- BrowserRenderer/Program.cs
+++ BrowserRenderer/Program.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.OffScreen;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace BrowserRenderer
 {
     class Program
     {
+        private static string CefAssemblyPath => Path.Combine(
+            AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+            Environment.Is64BitProcess ? "x64" : "x86");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Renderer running");
-            Console.Error.WriteLine("This is in the error stream");
+            Console.WriteLine("Render process running.");
+
+            AppDomain.CurrentDomain.AssemblyResolve += CustomAssemblyResolver;
+
+            Init();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void Init()
+        {
+            var settings = new CefSettings()
+            {
+                BrowserSubprocessPath = Path.Combine(CefAssemblyPath, "CefSharp.BrowserSubprocess.exe"),
+            };
+
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+
+            Console.WriteLine("CEF INITIALISED");
+        }
+
+        private static Assembly CustomAssemblyResolver(object sender, ResolveEventArgs args)
+        {
+            if (!args.Name.StartsWith("CefSharp")) { return null; }
+
+            var assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+            var assemblyPath = Path.Combine(CefAssemblyPath, assemblyName);
+
+            if (!File.Exists(assemblyPath))
+            {
+                Console.Error.WriteLine("Could not find assembly `{0}` at search path `{1}`", assemblyName, assemblyPath);
+                return null;
+            }
+
+            return Assembly.LoadFile(assemblyPath);
         }
     }
 }
