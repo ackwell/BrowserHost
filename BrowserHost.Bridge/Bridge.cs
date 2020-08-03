53b90c19ea50aa7a5c8d0b5be804bdb0928d4c75
ackwell <saxon@ackwell.com.au>
2020-08-03 22:06:12 +1000

    Call back without threading if WH already set

--- BrowserHost.Bridge/Bridge.cs
+++ BrowserHost.Bridge/Bridge.cs
@@ -8,12 +8,21 @@ namespace BrowserHost.Bridge
 	{
 		public static void OnReady(Action callback)
 		{
+			// Spin up a reference to the wait handle the main plugin will use to signal its ready state
+			var pid = Process.GetCurrentProcess().Id;
+			var waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset, $"BrowserHostBridgeReady{pid}");
+
+			// Do an inline check for the handle to see if it's open - if it is, we can skip threading for this.
+			var ready = waitHandle.WaitOne(0);
+			if (ready)
+			{
+				callback();
+				return;
+			}
+
 			// Boot up a new thread to block on waiting for the ready signal
-			// TODO: Worth booting the wait handle before thread and doing a check?
 			var thread = new Thread(() =>
 			{
-				var pid = Process.GetCurrentProcess().Id;
-				var waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset, $"BrowserHostBridgeReady{pid}");
 				// TODO: Drop this to like 5-10s, handle error if BH not avail
 				waitHandle.WaitOne(Timeout.Infinite);
 				callback();
